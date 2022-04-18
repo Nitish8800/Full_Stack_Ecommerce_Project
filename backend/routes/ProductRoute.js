ad523ac932e40fffec3280385da5a69539cb341e
Nitish8800 <nitishfea120784@gmail.com>
2022-04-19 01:17:23 +0530

    working on admin

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -5,15 +5,34 @@ const {
   updateProduct,
   deleteProduct,
   getSingleProduct,
+  createProductReview,
+  getSingleProductReviews,
+  deleteReview,
 } = require("../controller/ProductController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(createProduct);
+
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllProducts);
+
+router
+  .route("/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
 router
   .route("/product/:id")
-  .put(updateProduct)
-  .delete(deleteProduct)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
   .get(getSingleProduct);
+
+router.route("/product/review").post(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getSingleProductReviews)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
 
 module.exports = router;
