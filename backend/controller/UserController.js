d8977be2abcdd7af0b05be187be54728408a7777
Nitish8800 <nitishfea120784@gmail.com>
2022-04-19 00:45:44 +0530

    done till logout

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -28,14 +28,7 @@ exports.createUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  //   sendToken(user, 200, res);
-
-  const token = user.getJwtToken();
-
-  res.status(201).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
 });
 
 // Login User
@@ -43,7 +36,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new ErrorHandler("Please enter the email & password", 400));
+    return next(new ErrorHandler("Please enter the Email & Password", 400));
   }
 
   const user = await User.findOne({ email }).select("+password");
@@ -74,7 +67,7 @@ exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Log out success",
-  });
+  }); 
 });
 
 // Forgot password
